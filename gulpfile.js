b74ba260c3fccb3317b519f38e6d6a1a51344a3c
SamoylenkoDarina <samdarigo91@gmail.com>
2016-10-08 00:15:29 +0300

    Implemented SCSS Functionality

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require ('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+    return gulp.src('./frontend/stylesheets/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('test', ['sass'], function() { console.log('Sass compilation has been completed') });
+ 
+gulp.task('sass:watch', function () {
+    gulp.watch('./frontend/stylesheets/**/*.scss', ['test']);
+});
