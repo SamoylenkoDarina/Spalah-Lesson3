727d3532ab0c49c7b80c3efb9f228bb0bf2325dc
SamoylenkoDarina <samdarigo91@gmail.com>
2016-10-22 00:49:40 +0300

    Partially implemented

---
+++ frontend/js/dropdown.js
@@ -0,0 +1,26 @@
+(function(){
+	$( document ).ready(function() {
+		$('.header__dropdown').hide();
+		$('.header__item-dropdown').on('mouseenter', function(e) {
+			var that = $(this);
+			that
+			.find('.header__nav-item')
+			.addClass('header__nav-item__hover');
+			that
+			.find('.header__dropdown')
+			.stop()
+			.slideDown();
+		});
+		$('.header__item-dropdown').on('mouseleave', function() {
+			var that = $(this);
+			that
+			.find('.header__dropdown')
+			.stop()
+			.slideUp(function() {
+				that
+				.find('.header__nav-item')
+				.removeClass('header__nav-item__hover');
+			});
+		});
+	});
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,58 @@
 'use strict';
- 
-var gulp = require('gulp');
-var sass = require ('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
+
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    browserSync     = require('browser-sync'),
+    imagemin        = require('gulp-imagemin'),
+    watch           = require('gulp-watch'),
+    autoprefixer    = require('gulp-autoprefixer'),    
+    gulpFilter      = require('gulp-filter'),
+    mainBowerFiles  = require('gulp-main-bower-files'),
+    flatten         = require('gulp-flatten');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./frontend/stylesheets/**/*.scss', ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
 
 gulp.task('sass', function () {
     return gulp.src('./frontend/stylesheets/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 15 versions']
         }))
+        .pipe(browserSync.stream())
         .pipe(sourcemaps.write())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('test', ['sass'], function() { console.log('Sass compilation has been completed') });
- 
-gulp.task('sass:watch', function () {
-    gulp.watch('./frontend/stylesheets/**/*.scss', ['test']);
+gulp.task('image:build', function(){
+    return watch(['./frontend/images/**/*.jpg', './frontend/images/**/*.png', './frontend/images/**/*.svg', './frontend/images/**/*.gif'], function () {
+        gulp.src(['./frontend/images/**/*.jpg', './frontend/images/**/*.png', './frontend/images/**/*.svg', './frontend/images/**/*.gif'])
+            .pipe(imagemin())
+            .pipe(gulp.dest('./public/images'));
+    });
 });
+
+gulp.task('fonts', function(){
+    return gulp.src(['./frontend/fonts/**/*.eot', './frontend/fonts/**/*.ttf', './frontend/fonts/**/*.otf', './frontend/fonts/**/*.svg', './frontend/fonts/**/*.woff'])
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('main-bower-files', function() {
+    var filterJS = gulpFilter('**/*.js', { restore: true });
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles( ))
+        .pipe(filterJS)
+        .pipe(flatten())
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('default', ['browser-sync', 'sass', 'image:build', 'main-bower-files', 'fonts']);
